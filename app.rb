fa099d3f01cb53f1722d62d08cfdec7a66bd3ef6
Nick Inman <nick@theoatmeal.com>
2019-01-22 02:07:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get "/reversename/:name" do 
+  get "/reversename/:name" do
     @name = params[:name]
     @name.reverse
-  end 
+  end
 
+  get "/square/:number" do
+    @number = params[:number]
+    "#{@number * @number}"
+  end
 end
