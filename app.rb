a283859e49d50e054f937c34117b30e20d86b42c
Nick Inman <nick@theoatmeal.com>
2019-01-22 02:35:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     phrase = params.each do |key,value|
-      phrase += value
+      phrase += "#{value} "
       binding.pry
 
     end
