612e2a42fb3b563d388c85fd0aad5e375c3d5d3b
Nick Inman <nick@theoatmeal.com>
2019-01-22 02:11:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,6 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    do.times(@number) "#{@phrase}"
+    @number.times("#{@phrase}")
   end
 end
