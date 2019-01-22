4e514d2d0ebdd708efd247995fe7b6ef9b69fbd7
Nick Inman <nick@theoatmeal.com>
2019-01-22 02:44:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,11 +21,12 @@ class App < Sinatra::Base
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     phrase = []
     params.each.with_index do |key,value,index|
+      binding.pry
       if value < params.count
         phrase << "#{value} "
-      else 
+      else
         phrase << "#{value}."
-      end 
+      end
       phrase.join
     end
     binding.pry
