3c35f0989762f9d83fb6c4f6e49b37efd3ea0321
Nick Inman <nick@theoatmeal.com>
2019-01-22 02:58:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,12 +34,11 @@ class App < Sinatra::Base
 
   get "/:operation/:number1/:number2" do
     @operator = params[:operation]
-    @num1 = params[:number1]
-    @num2 = params[:number2]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
 
     if @operator == "add"
-      binding.pry
-      "#{@num1 + @num2}"
+      @num1 + @num2
     end
     #binding.pry
 
